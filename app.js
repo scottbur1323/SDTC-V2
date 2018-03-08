16c7b313dcc24f25223ba530cdd239b112ff2a3f
Chance Burke <burke@Chances-MacBook-Pro.local>
2018-03-07 20:02:53 -0700

    test commit

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const cors = require('cors')
-const data = require('./data/cohorts.json')
+const data = require('./data/instructors')
 const app = express()
 const port = process.env.PORT || 3000
 app.use(cors())
 
 function findById(data, id) {
-  var tempObject = {}
-  var error = "false"
+  var error = "falsenode "
   for (i=0;i<data.length;i++) {
     if (id == data[i].id) {
       return data[i]
@@ -17,16 +16,14 @@ function findById(data, id) {
 }
 
 app.get('/', function(req, res) {
-  res.send({data})
+  res.json({data})
 })
 
 app.get("/:id", function (req, res) {
-  var cohortById = findById(data, req.params.id)
-  if (cohortById) {
-    res.send({"data": cohortById})
+  var instuctorById = findById(data, req.params.id)
+  if (instuctorById) {
+    res.json({"data": instuctorById})
   } else res.status(404).json({error: {"message": "No record found!"}})
 })
-
-
 
 app.listen(port)
